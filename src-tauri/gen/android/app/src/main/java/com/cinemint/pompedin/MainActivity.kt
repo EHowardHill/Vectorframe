2f74623b642c7863d45288496cb8c96c92aea24b
Ethan Hill <ethan.hill1999@outlook.com>
2026-03-11 16:33:50 -0500

    Android init

---
+++ src-tauri/gen/android/app/src/main/java/com/cinemint/pompedin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cinemint.pompedin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
